3ecd96e6bd8e9218f9e1b66d636b029250fefca5
Giovanni Pau <giovannipau08@gmail.com>
2020-03-23 17:36:48 +0100

    Task of gaussian and uniform matrixes solved

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import numpy as np
 
 
 def uniform(n_rows, n_cols):
-    pass
+    m = np.random.uniform(size=(n_rows, n_cols))
+    return m
 
 
 def gaussian(n_rows, n_cols):
-    pass
+    m = np.random.normal(size=(n_rows, n_cols))
+    return m
+
+
+if __name__ == '__main__':
+    print('Gaussian:\n', gaussian(3, 3))
+    print('\nUniform:\n', uniform(3, 3))
